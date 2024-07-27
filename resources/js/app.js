2ee27c01ac140d0a582720eb3aa577305c24007f
Diego_Laura <diego.laura.cabbage@gmail.com>
2024-07-27 13:41:55 -0500

    La vista de historia paciente ya esta etsa hecha solo faltaria jalar los datos de los campos

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,6 +25,7 @@ import ConsultasDiarias from './admin/reportes/ConsultasDiarias.js';
 import TerapiasDiarias from './admin/reportes/TerapiasDiarias.js';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import HistoriaPaciente from './admin/Paciente/HistoriaPaciente.js';
 
 function AppRouter() {
     return (
@@ -45,6 +46,8 @@ function AppRouter() {
                     <Route path="/crear-paciente" element={<Sidebar component={<CrearPaciente />} />} />
                     <Route path="/usuarios" element={<Sidebar component={<Usuarios />} />} />
                     <Route path="/lista-pacientes" element={<Sidebar component={<ListaPaciente />} />} />
+                    
+                    <Route path="/historia-paciente" element={<Sidebar component={<HistoriaPaciente />} />} />
                     <Route path="/recetas" element={<Sidebar component={<VerRecetas />} />} />
                     <Route path="/crear-receta" element={<Sidebar component={<CrearReceta />} />} />
                     <Route path="/reportes" element={<Sidebar component={<VerRecetas />} />} />
