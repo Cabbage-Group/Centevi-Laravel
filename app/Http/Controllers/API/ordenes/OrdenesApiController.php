d25c5e94b36fbfacefe2ef5f2db6d57227658253
cmanuelcabbage <cmanuel.cabbage@gmail.com>
2025-01-20 16:21:38 -0500

    2001 Ordenes: Nro Orden

--- app/Http/Controllers/API/ordenes/OrdenesApiController.php
+++ app/Http/Controllers/API/ordenes/OrdenesApiController.php
@@ -986,7 +986,7 @@ class OrdenesApiController extends Controller
       ->first();
     $data = [
       'fecha_solicitud' => $orden['created_at'],
-      'nro_orden' => $orden['nro_orden'],
+      'nro_orden' => $orden['nro_orden_id'],
       'lenteContacto' => false,
       'esfera_od' => $orden['esfera_od'],
       'cilindro_od' => $orden['cilindro_od'],
@@ -1022,7 +1022,9 @@ class OrdenesApiController extends Controller
       'tratamientos_oi' => $orden['tratamientos_oi'],
       'tratamientos_od' => $orden['tratamientos_od'],
       'sucursal' => $orden['nombre'] ?? '',
-      'nombres_apellidos_paciente' => ($orden['nombres'] ?? '') . ' ' . ($orden['apellidos'] ?? '')
+      'nombres_apellidos_paciente' => ($orden['nombres'] ? explode(' ', trim($orden['nombres']))[0] : '') 
+      . ' ' 
+      . ($orden['apellidos'] ? explode(' ', trim($orden['apellidos']))[0] : '') 
     ];
 
     $pdf = Pdf::loadView('pdf/ordenPdf', $data);
