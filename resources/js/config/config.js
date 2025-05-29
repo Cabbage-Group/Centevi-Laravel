490249d25cf617399402d2122a6940f0d9b0787e
root <gvilcacisne@gmail.com>
2025-05-28 23:51:27 -0500

    config

--- resources/js/config/config.js
+++ resources/js/config/config.js
@@ -1,5 +1,6 @@
 
-const API = "http://127.0.0.1:8000/api";
+// const API = "http://127.0.0.1:8000/api";
+const API = "https://agenda.centevi.digital/api";
 // const API = "https://centevi-laravel.centevi.digital/api";
 
 export default API
